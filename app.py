447f1a18f6882467ec2666aac65d037d68c0c0d4
尹庆文 <Yinqingwen@gmail.com>
2021-11-30 14:00:23 +0800

    add compoent

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import requests
+from requests.packages import urllib3
+
 app = Flask(__name__)
+urllib3.disable_warnings()
 
 @app.route("/")
-def home():
-    return "Hello, Flask! 你好， Flask!"
-    
+def home():  
+    req = requests.get('https://grwsyw.bjgjj.gov.cn/ish/',verify=False)
+    print(req.text)
