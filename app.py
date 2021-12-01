8ee04b2cb5a6f1fc3ec6ac76216e339881ad95e6
尹庆文 <Yinqingwen@gmail.com>
2021-12-01 18:02:12 +0800

    20211201

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
 import requests
-from requests.models import Response
-from requests.packages import urllib3
 
 app = Flask(__name__)
-urllib3.disable_warnings()
 
 @app.route("/")
-def home():  
-    url = "https://cl.3637x.xyz/index.php"
-    response1 = requests.get(url)
-    return 'response1'     
+def home():
+    """获取免翻墙地址"""  
+    url = "https://cl.5837x.xyz/index.php/"
+    res = requests.get(url,verify=False)
+    """将网页编码强制设置为GBK"""
+    res.encoding = "gbk"
+    soup = BeautifulSoup(res.text)
+    for link in soup.find_all('a'):
+        print(link.get())
+    site_list = [1,2,3,4]
+    return render_template("index.html",site_list = site_list)
